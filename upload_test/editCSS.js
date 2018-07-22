3f5490f94482107e93c987bb131b02b1dcf7a587
Michael Klein <miketheklein@gmail.com>
2018-07-22 13:53:56 -0700

    added border and editable text

--- upload_test/editCSS.js
+++ upload_test/editCSS.js
@@ -2,12 +2,12 @@ const editButton = document.querySelector('.postit__edit');
 const postit = document.querySelector('.draggable');
 
 function randomColor() {
-  let randomR = Math.floor(Math.random() * 255)
-  let randomG = Math.floor(Math.random() * 255)
-  let randomB = Math.floor(Math.random() * 255)
-  return `rgb(${randomR}, ${randomG}, ${randomB})`
+  let randomRGB = () => Math.floor(Math.random() * 255)
+  return `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`
 }
 
 editButton.onclick = function() {
-  postit.setAttribute("style", `background: ${randomColor()}`)
+  postit.setAttribute("style", 
+  `background: ${randomColor()};
+  border: 5px solid ${randomColor()};`)
 }
