6c9afa807039ba9fbae39c3cf49b0b2f052bb324
Michael Klein <miketheklein@gmail.com>
2018-07-22 14:20:45 -0700

    additional functionality to colors and borders

--- upload_test/editCSS.js
+++ upload_test/editCSS.js
@@ -4,13 +4,26 @@ const postitContent = document.querySelector('.postit__content');
 
 function randomColor() {
   let randomRGB = () => Math.floor(Math.random() * 255)
-  return `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`
+  let r = randomRGB();
+  let g = randomRGB();
+  let b = randomRGB();
+  let fontColor = (r * 0.299 + g * 0.587 + b * 0.114) > 186
+    ? '#000000'
+    : '#FFFFFF';
+  let borderColor = (`rgb(${255 - r}, ${255 - g}, ${255 - b});`)
+  return [`rgb(${r}, ${g}, ${b});`, borderColor, fontColor]
 }
 
+let colorTheme;
+
+console.log('theme', colorTheme)
 editButton.onclick = function() {
+  colorTheme = randomColor();
   postit.setAttribute("style", 
-    `background: ${randomColor()};
-    border: 5px solid ${randomColor()};`
+    `background: ${colorTheme[0]}
+    border: 5px solid ${colorTheme[1]}`
   )
-  postitContent.setAttribute("style", `color: ${randomColor()}`)
+  postitContent.setAttribute("style", `color: ${colorTheme[2]};`)
+  colorTheme = null;
 }
+
